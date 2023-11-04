f12b66657a85a58ab078f34a6fe5ea11900f7ba7
pcrainer38 <pammie_78704@yahoo.com>
2023-11-03 23:38:38 -0500

    added public and views folders

---
+++ public/js/login.js
@@ -0,0 +1,53 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect values from login form
+    const userName = document.querySelector('#userName-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        // Send a POST to API endpoint
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ userName, password}),
+            headers: { 'Content-Type': 'application/json '},
+        });
+
+        if (response.ok) {
+            // If successful, redirect the brow to the profile page
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const userName = document.querySelector('#userName-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (userName && email && password) {
+        const response = await fetch('api/users', {
+            method: 'POST',
+            body: JSON.stringify({ userName, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
